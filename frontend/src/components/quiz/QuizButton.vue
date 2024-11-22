43c7a85bb11a15371d11fe35b4a3f4f49b9d21a7
puranium <63571947+puranium235@users.noreply.github.com>
2024-11-22 10:20:53 +0900

    FE: 퀴즈 결과 조회 화면 (#17)
    * feat: 퀴즈 결과 화면
    
    * style: hover 삭제
    
    * ìfeat: 상식 투표 ê결과화면
    
    * feat: 그래프 차오르는 애니메이션 구현
    
    * feat: 커서 모양 수정
    
    * feat: 다ì다음 문제 부르기
    
    * fix: 투표바에 v-show 추가
    
    * fix: 투표수가 0일 때 ì예 퍼센트 0으로 고정

--- frontend/src/components/quiz/QuizButton.vue
+++ frontend/src/components/quiz/QuizButton.vue
@@ -1,25 +1,73 @@
 <template>
-  <button class="button" :class="{ selected }">
-    {{ value }}
+  <button class="button" :style="boxStyle">
+    <span>{{ value }}</span>
+    <span>{{ isResult ? ` ${showPercent.toFixed(1)} %` : ''}}</span>
+    <div v-show="isResult" class="graph" :style="graphStyle"></div>
   </button>
 </template>
 
 <script setup>
-defineProps({
+import { computed, ref, watch } from 'vue'
+
+const props = defineProps({
   value: String,
   selected: Boolean,
+  total: Number,
+  votes: Number,
+  isResult: Boolean,
 })
+
+const percent = computed(() => props.total === 0 ? 0 : (props.votes / props.total * 100).toFixed(1))
+const showPercent = ref(0)
+
+const percentInterval = () => {
+  const timer = setInterval(() => {
+    showPercent.value += 0.35;
+    if (showPercent.value > percent.value) {
+      showPercent.value = Number(percent.value)
+      clearInterval(timer)
+    }
+  }, 8)
+}
+
+watch(() => props.isResult, (newValue) => {
+  if (newValue === true) {
+    showPercent.value = 0
+    percentInterval()
+  }
+})
+
+const boxStyle = computed(() => {
+  return {
+    border: props.isResult && props.selected ? "2px solid #2196F3" : "1px solid #000000",
+    boxShadow: props.isResult && props.selected ? "0 0 10px #2196F3" : "none",
+    cursor: props.isResult ? 'default' : 'pointer'
+  }
+})
+
+const graphStyle = computed(() => {
+  return {
+    height: `${ showPercent.value }%`,
+    backgroundColor: props.selected ? "#E3F2FD" : "#E9E9E9",
+  }
+})
+
 </script>
 
 <style scoped>
 .button {
+  position: relative;
   flex-basis: 50%;
   aspect-ratio: 2;
   border: 1px solid #000000;
 
   font-size: 24px;
 }
-.button:hover, .selected {
-  border: 2px solid;
+.graph {
+  position: absolute;
+  z-index: -1;
+  bottom: 0;
+  left: 0;
+  width: 100%;
 }
 </style>
--- frontend/src/components/quiz/QuizOption.vue
+++ frontend/src/components/quiz/QuizOption.vue
@@ -1,26 +1,77 @@
 <template>
-  <div class="answer" :class="{ selected }" >
-    {{ option }}
+  <div class="answer" :class="{ selected }" :style="boxStyle" @mouseup="handleMouseUp">
+    <span class="option">{{ option }}</span>
+    <div v-show="isResult" class="graph" :style="graphStyle"></div>
+    <span v-show="isResult">{{ showPercent.toFixed(1) }}%</span>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed, ref, watch } from 'vue'
+
+const props = defineProps({
   option: String,
   selected: Boolean,
+  total: Number,
+  votes: Number,
+  isResult: Boolean,
+  isAnswer: Boolean
 })
+
+const percent = computed(() => props.total === 0 ? 0 : (props.votes / props.total * 100).toFixed(1))
+const showPercent = ref(0)
+
+const percentInterval = () => {
+  const timer = setInterval(() => {
+    showPercent.value += 0.35;
+    if (showPercent.value > percent.value) {
+      showPercent.value = Number(percent.value)
+      clearInterval(timer)
+    }
+  }, 8)
+}
+
+watch(() => props.isResult, (newValue) => {
+  if (newValue === true) {
+    showPercent.value = 0
+    percentInterval()
+  }
+})
+
+const boxStyle = computed(() => {
+  return {
+    border: (props.isResult ? ((props.isAnswer ? '#4CAF50' : '#D14D72') + " 2px") : '#000000 1px') + " solid",
+    boxShadow: (props.selected ? "0 0 10px" : "0 0 0") + " " + (props.isResult ? (props.isAnswer ? "#4CAF50" : "#D14D72") : '#616161'),
+    cursor: props.isResult ? 'default' : 'pointer'
+  }
+})
+
+const graphStyle = computed(() => {
+  return {
+    width: `${ showPercent.value }%`,
+    backgroundColor: props.isAnswer ? '#DFF2E1' : '#FCC8D1'
+  }
+})
+
+
 </script>
 
 <style scoped>
 .answer {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
   height: 50px;
   line-height: 25px;
   padding: 12px;
   font-size: 16px;
-  border: 1px solid #000000;
-  cursor: pointer;
 }
-.answer:hover, .selected {
-  border: 2px solid;
+.graph {
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
 }
 </style>
